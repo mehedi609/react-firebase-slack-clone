1f749ec7364d4ca1b1eb21ba3280ddd724c2fc72
Mehedi Hasan <mehedi609@gmail.com>
2021-04-26 22:31:40 +0600

    validate register form

--- src/component/Auth/Register.jsx
+++ src/component/Auth/Register.jsx
@@ -1,102 +1,165 @@
-import React, {Component} from 'react';
-import {Button, Form, Grid, Header, Icon, Message, Segment} from "semantic-ui-react";
-import {Link} from "react-router-dom";
-import {_firebase} from "../../config/firebase";
-
+import React, { Component } from 'react';
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Icon,
+  Message,
+  Segment,
+} from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { _firebase } from '../../config/firebase';
 
 class Register extends Component {
-    state = {
-        username: "",
-        email: "",
-        password: "",
-        passwordConfirmation: ""
-    };
+  state = {
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
+    errors: [],
+  };
 
-    handleChange = (event) => {
-        const {name, value} = event.target;
-        this.setState({[name]: value})
-    };
+  isFormValid = () => {
+    let errors = [];
+    let error;
 
-    handleSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            const createdUser = await _firebase
-                .auth()
-                .createUserWithEmailAndPassword(this.state.email, this.state.password)
-            console.log(createdUser)
-        } catch (e) {
-            console.log(e.message)
-        }
+    if (this.isFormEmpty(this.state)) {
+      error = { message: 'Fill in all fields' };
+      this.setState({ errors: errors.concat(error) });
+      return false;
+    } else if (!this.isPasswordValid(this.state)) {
+      error = { message: 'Password is invalid' };
+      this.setState({ errors: [...errors, error] });
+      return false;
+    } else {
+      return true;
     }
+  };
 
-    render() {
-        const {username, email, password, passwordConfirmation} = this.state
+  isFormEmpty = ({ username, email, password, passwordConfirmation }) => {
+    return (
+      !username.length ||
+      !email.length ||
+      !password.length ||
+      !passwordConfirmation.length
+    );
+  };
 
-        return (
-            <Grid textAlign="center" verticalAlign="middle" className="app">
-                <Grid.Column style={{ maxWidth: 450 }}>
-                    <Header as="h2" icon color="orange" textAlign="center">
-                        <Icon name="puzzle piece" color="orange" />
-                        Register for DevChat
-                    </Header>
-                    <Form size="large" onSubmit={this.handleSubmit}>
-                        <Segment stacked>
-                            <Form.Input
-                                fluid
-                                name="username"
-                                value={username}
-                                icon="user"
-                                iconPosition="left"
-                                placeholder="Username"
-                                onChange={this.handleChange}
-                                type="text"
-                            />
-
-                            <Form.Input
-                                fluid
-                                name="email"
-                                value={email}
-                                icon="mail"
-                                iconPosition="left"
-                                placeholder="Email Address"
-                                onChange={this.handleChange}
-                                type="email"
-                            />
-
-                            <Form.Input
-                                fluid
-                                name="password"
-                                value={password}
-                                icon="lock"
-                                iconPosition="left"
-                                placeholder="Password"
-                                onChange={this.handleChange}
-                                type="password"
-                            />
-
-                            <Form.Input
-                                fluid
-                                name="passwordConfirmation"
-                                value={passwordConfirmation}
-                                icon="repeat"
-                                iconPosition="left"
-                                placeholder="Password Confirmation"
-                                onChange={this.handleChange}
-                                type="password"
-                            />
-
-                            <Button color="orange" fluid size="large">
-                                Submit
-                            </Button>
-                        </Segment>
-                    </Form>
-                    <Message>
-                        Already a user? <Link to="/login">Login</Link>
-                    </Message>
-                </Grid.Column>
-            </Grid>
-        );
+  isPasswordValid = ({ password, passwordConfirmation }) => {
+    if (password.length < 6 || passwordConfirmation.length < 6) {
+      return false;
+    } else if (password !== passwordConfirmation) {
+      return false;
     }
+    return true;
+  };
+
+  displayErrors = (errors) =>
+    errors.map((error, i) => <p key={i}>{error.message}</p>);
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = async (event) => {
+    if (this.isFormValid()) {
+      event.preventDefault();
+      try {
+        const createdUser = await _firebase
+          .auth()
+          .createUserWithEmailAndPassword(
+            this.state.email,
+            this.state.password
+          );
+        console.log(createdUser);
+      } catch (e) {
+        console.log(e.message);
+      }
+    }
+  };
+
+  render() {
+    const {
+      username,
+      email,
+      password,
+      passwordConfirmation,
+      errors,
+    } = this.state;
+
+    return (
+      <Grid textAlign="center" verticalAlign="middle" className="app">
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" icon color="orange" textAlign="center">
+            <Icon name="puzzle piece" color="orange" />
+            Register for DevChat
+          </Header>
+          <Form size="large" onSubmit={this.handleSubmit}>
+            <Segment stacked>
+              <Form.Input
+                fluid
+                name="username"
+                value={username}
+                icon="user"
+                iconPosition="left"
+                placeholder="Username"
+                onChange={this.handleChange}
+                type="text"
+              />
+
+              <Form.Input
+                fluid
+                name="email"
+                value={email}
+                icon="mail"
+                iconPosition="left"
+                placeholder="Email Address"
+                onChange={this.handleChange}
+                type="email"
+              />
+
+              <Form.Input
+                fluid
+                name="password"
+                value={password}
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                onChange={this.handleChange}
+                type="password"
+              />
+
+              <Form.Input
+                fluid
+                name="passwordConfirmation"
+                value={passwordConfirmation}
+                icon="repeat"
+                iconPosition="left"
+                placeholder="Password Confirmation"
+                onChange={this.handleChange}
+                type="password"
+              />
+
+              <Button color="orange" fluid size="large">
+                Submit
+              </Button>
+            </Segment>
+          </Form>
+          {errors.length > 0 && (
+            <Message error>
+              <h3>Error</h3>
+              {this.displayErrors(errors)}
+            </Message>
+          )}
+          <Message>
+            Already a user? <Link to="/login">Login</Link>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    );
+  }
 }
 
 export default Register;
